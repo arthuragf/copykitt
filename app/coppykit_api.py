1d6f150932b341f6404509795177438ab16f8fb8
Arthur Faria <arthur.agf@gmail.com>
2025-07-06 23:30:59 -0300

    Fix prompt validation

--- app/coppykit_api.py
+++ app/coppykit_api.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, HTTPException, Depends, Query
+from fastapi import FastAPI, Query
 from coppykit import generate_branding_snippet, generate_branding_keywords
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional, Annotated
 import os
 from dotenv import load_dotenv
 
@@ -16,30 +16,18 @@ class BrandingResponse(BaseModel):
 
 app = FastAPI()
 
-def validate_prompt_length(prompt: str = Query(...)) -> str:
-    print(prompt)
-    if len(prompt) > MAX_USER_INPUT_LENGTH:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Input length is too long. Must be under {MAX_USER_INPUT_LENGTH}"
-        )
-    return prompt
-
 @app.get("/generate_snippet", response_model=BrandingResponse)
-@validate_prompt_length
-async def generate_snippet(prompt: str = Depends(validate_prompt_length)):
+async def generate_snippet(prompt: Annotated[str, Query(..., max_length=32, min_length=3)]):
     snippet = generate_branding_snippet(prompt)
     return {"snippet": snippet, "keywords": []}
 
 @app.get("/generate_keywords", response_model=BrandingResponse)
-@validate_prompt_length
-async def generate_keywords(prompt: str = Depends(validate_prompt_length)):
+async def generate_keywords(prompt: Annotated[str, Query(..., max_length=32, min_length=3)]):
     keywords = generate_branding_keywords(prompt)
     return {"snippet": None, "keywords": keywords}
 
 @app.get("/generate_snippet_and_keywords", response_model=BrandingResponse)
-@validate_prompt_length
-async def generate_snippet_and_keywords(prompt: str = Depends(validate_prompt_length)):
+async def generate_snippet_and_keywords(prompt: Annotated[str, Query(..., max_length=32, min_length=3)]):
     snippet = generate_branding_snippet(prompt)
     keywords = generate_branding_keywords(prompt)
     return {"snippet": snippet, "keywords": keywords}
