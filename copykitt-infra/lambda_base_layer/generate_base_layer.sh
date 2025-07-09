62e04bd81199ac757ce534fa833e28c6509066b7
Arthur Faria <arthur.agf@gmail.com>
2025-07-08 22:04:32 -0300

    Coomit to migrate project to linux

--- copykitt-infra/lambda_base_layer/generate_base_layer.sh
+++ copykitt-infra/lambda_base_layer/generate_base_layer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -e
+# Generates a base layer for the Lambda function
 
-# Remove container antigo (se existir)
+# Remove the container first (if it exists)
 docker rm -f layer-container 2>/dev/null || true
 
-# Build da imagem
+# Build the base layer
 docker build -t base-layer .
 
-# Cria container
+# rename it to base-layer
 docker run --name layer-container base-layer
 
-# Copia o zip gerado
-docker cp layer-container:/opt/layer.zip . && echo "✅ layer.zip gerado com sucesso!"
+# Copy the generated zip artifact so our CDK can use it
+docker cp layer-container:layer.zip . && echo "Created layer.zip with updated base layer."
