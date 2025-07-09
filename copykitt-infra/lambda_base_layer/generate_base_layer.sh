a11d42045fa1d7b2987f0bad22ce737ad584d0f2
Arthur Faria <arthur.agf@gmail.com>
2025-07-08 22:34:34 -0300

    Fix the partial cdk scripts

--- copykitt-infra/lambda_base_layer/generate_base_layer.sh
+++ copykitt-infra/lambda_base_layer/generate_base_layer.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-# Generates a base layer for the Lambda function
-
-# Remove the container first (if it exists)
-docker rm -f layer-container 2>/dev/null || true
-
-# Build the base layer
 docker build -t base-layer .
-
-# rename it to base-layer
 docker run --name layer-container base-layer
-
-# Copy the generated zip artifact so our CDK can use it
-docker cp layer-container:layer.zip . && echo "Created layer.zip with updated base layer."
+docker cp layer-container:/var/task/layer.zip ./layer.zip
+docker rm -f layer-container
