b263c7b5f2aadd72a3e7950bcc24892585ac36a1
Arthur Faria <arthur.agf@gmail.com>
2025-07-10 00:14:51 -0300

    Add env imports for gemini api key

--- copykitt-infra/lambda_base_layer/generate_base_layer.sh
+++ copykitt-infra/lambda_base_layer/generate_base_layer.sh
@@ -1,4 +1,6 @@
+set -e
+docker rm -f layer-container 2>/dev/null || true
 docker build -t base-layer .
 docker run --name layer-container base-layer
-docker cp layer-container:/var/task/layer.zip ./layer.zip
+docker cp base-layer:layer.zip ./layer.zip
 docker rm -f layer-container
